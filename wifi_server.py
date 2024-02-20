385ae93cd6d11739f2ce264e87116c82a34ac21b
jo6109072 <john.pan@mail.mcgill.ca>
2024-02-19 21:26:21 -0500

    fix server

--- wifi_server.py
+++ wifi_server.py
@@ -6,7 +6,7 @@ PORT = 65432          # Port to listen on (non-privileged ports are > 1023)
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
     s.bind((HOST, PORT))
     s.listen()
-
+    client, clientInfo = s.accept()
     try:
         while 1:
             client, clientInfo = s.accept()
